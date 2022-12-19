1b54f6dc6a08edd8c899997a5b2790396391e536
NamDevelop997 <namdevelop997@gmail.com>
2022-12-20 02:52:14 +0700

    show data in php

---
+++ Lession/Section 3/showData.php
@@ -0,0 +1,11 @@
+<?php
+
+    // show data equal Echo
+    echo "Hello My Friends";
+
+    $myArr = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+    // show data equal Prin_r()
+    echo "<pre>";
+    print_r($myArr);
+    echo "<pre>";
