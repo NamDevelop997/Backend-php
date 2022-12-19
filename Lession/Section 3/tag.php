302acefc7f498d00a9f63b2d9f6993b2ef28dad6
NamDevelop997 <namdevelop997@gmail.com>
2022-12-20 02:34:15 +0700

    Learning Backend Php init

---
+++ Lession/Section 3/tag.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using tag in PHP</title>
+</head>
+<body>
+    <h1>Hello My name is Nam, i'm learning <?php echo "PHP language" ?></h1>
+</body>
+</html>
